12f342a467358660aefd7b4d085ebc0236a9d044
Pea <pea@misfist.com>
2017-03-14 19:44:59 -0400

    Moved byline below post title on archive

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -15,6 +15,9 @@
 
 		<header class="entry-header">
 
+			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+			'</a></h2>' ); ?>
+
 			<?php if ( 'post' == get_post_type() ) : ?>
 
 				<div class="entry-meta">
@@ -22,9 +25,6 @@
 				</div><!-- .entry-meta -->
 
 			<?php endif; ?>
-
-			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-			'</a></h2>' ); ?>
 
 		</header><!-- .entry-header -->
 
