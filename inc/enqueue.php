9b084b4bcc729277f811ace2788f706856514d33
Pea <pea@misfist.com>
2017-03-18 20:21:05 -0400

    Dequeued JetPack styles

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -40,6 +40,9 @@ add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
 function theme_enqueue_styles() {
 
   wp_deregister_style( 'the-neverending-homepage' );
+  wp_deregister_style( 'sharedaddy' );
+  wp_deregister_style( 'sharing' );
+  wp_deregister_style( 'social-logos' );
 
 	// Get the theme data
   $the_theme = wp_get_theme();
