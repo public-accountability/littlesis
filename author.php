671fc526ac3c5979c014e9869d5fd12270806da6
Pea <pea@misfist.com>
2017-03-30 21:21:08 -0400

    Created author.php template

---
+++ author.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * The template for displaying author pages.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package understrap
+ * @subpackage littlesis
+ */
+
+get_header();
+
+$container   = get_theme_mod( 'understrap_container_type' );
+$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
+?>
+
+
+<div class="wrapper" id="author-wrapper">
+
+	<div class="<?php echo esc_html( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<!-- Do the left sidebar check -->
+			<?php get_template_part( 'global-templates/left-sidebar-check', 'none' ); ?>
+
+			<main class="site-main" id="main">
+
+				<?php $author = ( isset( $_GET['author_name'] ) ) ? get_user_by( 'slug', $author_name ) : get_userdata( intval( $author ) ); ?>
+
+				<header class="page-header author-header">
+
+					<?php if ( ! empty( $author->ID ) ) : ?>
+						<figure class="author-avatar">
+							<?php echo get_avatar( $author->ID ); ?>
+						</figure>
+					<?php endif; ?>
+
+					<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+					?>
+
+					<h3 class="author-posts"><?php esc_html_e( 'Posts by', 'littlesis' ); ?> <?php echo esc_html( $author->display_name ); ?></h2>
+
+				</header><!-- .page-header -->
+
+					<!-- The Loop -->
+					<?php if ( have_posts() ) : ?>
+						<?php while ( have_posts() ) : the_post(); ?>
+
+							<?php
+
+							/*
+							 * Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'loop-templates/content', get_post_format() );
+							?>
+
+						<?php endwhile; ?>
+
+					<?php else : ?>
+
+						<?php get_template_part( 'loop-templates/content', 'none' ); ?>
+
+					<?php endif; ?>
+
+					<!-- End Loop -->
+
+			</main><!-- #main -->
+
+			<!-- The pagination component -->
+			<?php// understrap_pagination(); ?>
+
+		</div><!-- #primary -->
+
+		<!-- Do the right sidebar check -->
+		<?php if ( 'right' === $sidebar_pos || 'both' === $sidebar_pos ) : ?>
+
+			<?php get_sidebar( 'right' ); ?>
+
+		<?php endif; ?>
+
+	</div> <!-- .row -->
+
+</div><!-- Container end -->
+
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
