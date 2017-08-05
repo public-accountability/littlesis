d332b2345b8d3d3e9841a384071e41a8bd1cc83f
Pea <pea@misfist.com>
2017-08-05 15:42:39 -0400

    Added Helper Functions
    Added helper function that gets most recent sticky post or the latest post.

---
+++ inc/helpers.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Littlesis helper functions
+ *
+ * @package understrap
+ * @subpackage littlesis
+ * @since 0.1.2
+ */
+
+/**
+ * Get Latest Sticky Post
+ * Get the most recent sticky post ID in an array
+ *
+ * @since 0.1.2
+ *
+ * @return array | null
+ */
+function littlesis_get_latest_sticky_post() {
+  $sticky = get_option( 'sticky_posts' );
+
+  $posts = get_posts( array(
+    'include'         => $sticky,
+    'posts_per_page'  => count( $sticky ),
+    'fields'          => 'ids'
+  ) );
+
+  if( !empty( $posts ) && !is_wp_error( $posts ) ) {
+    return array_slice( $posts, 0, 1 );
+  }
+
+  return null;
+}
+
+/**
+ * Get Featured Post
+ * If there are sticky posts, the featured post is the latest post; otherwise, it's the latest post
+ *
+ * @since 0.1.2
+ *
+ * @uses littlesis_get_latest_sticky_post()
+ *
+ * @return array | null
+ */
+function littlesis_get_featured_post() {
+  $sticky = littlesis_get_latest_sticky_post();
+  if( $sticky ) {
+    return $sticky;
+  } else {
+    $args = array(
+      'posts_per_page'      => 1,
+      'ignore_sticky_post'  => true,
+      'fields'              => 'ids'
+    );
+    return get_posts( $args );
+  }
+}
