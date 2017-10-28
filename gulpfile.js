aa8c0f5279a93327df099676af6daf4329ea0156
Pea <pea@misfist.com>
2017-10-28 17:50:54 -0400

    Updated assets

--- gulpfile.js
+++ gulpfile.js
@@ -251,4 +251,4 @@ gulp.task('clean-dist-product', function () {
   return del(['dist-product/**/*',]);
 });
 
-gulp.task( 'default', [ 'watch-bs', 'scss-for-dev', 'scripts', 'images' ], function() {});
+gulp.task( 'default', [ 'watch-bs', 'scss-for-dev', 'images' ], function() {});
