21b107e6fd3804ab4b034458fc284cf1028c080a
Pea <pea@misfist.com>
2017-10-28 14:13:13 -0400

    Changed grid image to thumbnail

--- loop-templates/content-grid.php
+++ loop-templates/content-grid.php
@@ -11,7 +11,7 @@
 
 	<header class="entry-header">
 
-		<?php littlesis_the_post_thumbnail( $post->ID, 'medium' ); ?>
+		<?php littlesis_the_post_thumbnail( $post->ID, 'thumbnail' ); ?>
 
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
 		'</a></h2>' ); ?>
